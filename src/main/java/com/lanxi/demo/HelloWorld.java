37faae792bfeea60251b487f84af95638ddb321d
wuxiaobo <wuxiaobo@188lanxi.com>
2018-06-05 18:55:58 +0800

    Initial commit

---
+++ src/main/java/com/lanxi/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.lanxi.demo;
+
+/**
+ * Created by wuxiaobo on 2018/6/5 0005.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.err.println("hellow world!");
+    }
+}
